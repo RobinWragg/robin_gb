0d96d79cff4ddc779a05b987a2b39ce925549218
Robin Wragg <robin.wragg@me.com>
2024-02-21 21:46:11 +0000

    Begin test_app

--- src/bin/test_app.rs
+++ src/bin/test_app.rs
@@ -1,4 +1,14 @@
+use robin_gb;
+use std::fs;
+
 // rwtodo: This will become a simple app that loads multiple instances of the emulator and loads a different game in each one.
 fn main() {
     println!("Hello, world!");
+    let d = std::env::current_dir();
+    let rom_file_data = fs::read("roms/Tetris.gb").unwrap();
+    let mut gb = robin_gb::load_rom_file(&rom_file_data[..]).unwrap();
+
+    for i in 0..100 {
+        let frame = gb.emulate_next_frame();
+    }
 }
