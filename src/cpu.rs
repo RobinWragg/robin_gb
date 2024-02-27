9fc544715240853b1da83dde9c16121278501a94
Robin Wragg <robin.wragg@me.com>
2024-02-27 21:27:58 +0000

    Refactor cpu and lcd modules, and implement CALL instruction

--- src/cpu.rs
+++ src/cpu.rs
@@ -3,9 +3,26 @@ use crate::interrupt;
 use crate::Memory;
 use crate::{bit, make_u16};
 
+mod instructions;
+
 //rwtodo: I can probably do something nifty with Rust attributes to make the "instruction" functions more ergonomic.
 
 // rwtodo: Apparently STOP is like HALT except the LCD is inoperational as well, and the "stopped" state is only exited when a button is pressed. Look for better documentation on it.
+
+fn stack_push(value_to_push: u16, sp: &mut u16, memory: &mut Memory) {
+    let bytes = value_to_push.to_le_bytes();
+    *sp -= 2;
+
+    // rwtodo: this can be memory.write_u16, right?
+    memory.write(*sp, bytes[0]);
+    memory.write(*sp + 1, bytes[1]);
+}
+
+fn stack_pop(sp: &mut u16, memory: &Memory) -> u16 {
+    let popped_value = memory.read_u16(*sp);
+    *sp += 2;
+    popped_value
+}
 
 fn print_instruction(pc: u16, memory: &mut Memory) {
     let opcode = memory.read(pc);
@@ -152,277 +169,6 @@ impl Registers {
     }
 }
 
-mod instructions {
-    use crate::cpu::Registers;
-
-    pub struct Finish {
-        pub pc_increment: i16,
-        pub elapsed_cycles: u8,
-    }
-
-    pub fn subtraction_produces_u8_full_carry(a: u8, b: u8) -> bool {
-        i16::from(a) - i16::from(b) < 0
-    }
-
-    pub fn addition_produces_u8_full_carry(a: u8, b: u8) -> bool {
-        i16::from(a) + i16::from(b) > 0xff
-    }
-
-    pub fn subtraction_produces_u8_half_carry(
-        a: u8,
-        b: u8,
-        register_f: u8,
-        include_carry: bool,
-    ) -> bool {
-        let optional_carry: i16 = if include_carry && (register_f & Registers::FLAG_CARRY) != 0 {
-            1
-        } else {
-            0
-        };
-
-        i16::from(a & 0x0f) - i16::from(b & 0x0f) - optional_carry < 0
-    }
-
-    pub fn addition_produces_u8_half_carry(
-        a: u8,
-        b: u8,
-        register_f: u8,
-        include_carry: bool,
-    ) -> bool {
-        let optional_carry: i16 = if include_carry && (register_f & Registers::FLAG_CARRY) != 0 {
-            1
-        } else {
-            0
-        };
-
-        i16::from(a & 0x0f) + i16::from(b & 0x0f) + optional_carry > 0x0f
-    }
-
-    pub fn inc_u8(value_to_increment: &mut u8, register_f: &mut u8, elapsed_cycles: u8) -> Finish {
-        if addition_produces_u8_half_carry(*value_to_increment, 1, *register_f, false) {
-            *register_f |= Registers::FLAG_HALFCARRY;
-        } else {
-            *register_f &= !Registers::FLAG_HALFCARRY;
-        }
-
-        *value_to_increment += 1;
-
-        if *value_to_increment != 0 {
-            *register_f &= !Registers::FLAG_ZERO;
-        } else {
-            *register_f |= Registers::FLAG_ZERO;
-        }
-
-        *register_f &= !Registers::FLAG_SUBTRACTION;
-
-        Finish {
-            pc_increment: 1,
-            elapsed_cycles,
-        }
-    }
-
-    pub fn xor(
-        xor_input: u8,
-        register_a: &mut u8,
-        register_f: &mut u8,
-        elapsed_cycles: u8,
-    ) -> Finish {
-        *register_a ^= xor_input;
-
-        if *register_a == 0 {
-            *register_f |= Registers::FLAG_ZERO;
-        } else {
-            *register_f &= !Registers::FLAG_ZERO;
-        }
-
-        *register_f &= !Registers::FLAG_SUBTRACTION;
-        *register_f &= !Registers::FLAG_HALFCARRY;
-        *register_f &= !Registers::FLAG_CARRY;
-
-        Finish {
-            pc_increment: 1,
-            elapsed_cycles,
-        }
-    }
-
-    fn or(
-        or_input: u8,
-        register_a: &mut u8,
-        register_f: &mut u8,
-        pc_increment: i16,
-        elapsed_cycles: u8,
-    ) -> Finish {
-        // rwtodo: I think pc_increment might always be 1, thereby allowing us to remove it as from the param list.
-
-        *register_a |= or_input;
-
-        if *register_a == 0 {
-            *register_f |= Registers::FLAG_ZERO;
-        } else {
-            *register_f &= !Registers::FLAG_ZERO;
-        }
-
-        *register_f &= !Registers::FLAG_SUBTRACTION;
-        *register_f &= !Registers::FLAG_HALFCARRY;
-        *register_f &= !Registers::FLAG_CARRY;
-
-        Finish {
-            pc_increment,
-            elapsed_cycles,
-        }
-    }
-
-    pub fn and(
-        and_input: u8,
-        register_a: &mut u8,
-        register_f: &mut u8,
-        pc_increment: i16,
-        elapsed_cycles: u8,
-    ) -> Finish {
-        *register_a &= and_input;
-
-        if *register_a == 0 {
-            *register_f |= Registers::FLAG_ZERO;
-        } else {
-            *register_f &= !Registers::FLAG_ZERO;
-        }
-
-        *register_f &= !Registers::FLAG_SUBTRACTION;
-        *register_f |= Registers::FLAG_HALFCARRY;
-        *register_f &= !Registers::FLAG_CARRY;
-
-        Finish {
-            pc_increment,
-            elapsed_cycles,
-        }
-    }
-
-    fn set(bit_to_set: u8, byte_to_set: &mut u8, elapsed_cycles: u8) -> Finish {
-        *byte_to_set |= 0x01 << bit_to_set;
-        Finish {
-            pc_increment: 1,
-            elapsed_cycles,
-        }
-    }
-
-    fn res(bit_to_reset: u8, byte_to_reset: &mut u8, elapsed_cycles: u8) -> Finish {
-        *byte_to_reset &= !(0x01 << bit_to_reset);
-        Finish {
-            pc_increment: 1,
-            elapsed_cycles,
-        }
-    }
-
-    fn bit(bit_to_check: u8, byte_to_check: u8, register_f: &mut u8, elapsed_cycles: u8) -> Finish {
-        if (byte_to_check & (0x01 << bit_to_check)) != 0 {
-            *register_f &= !Registers::FLAG_ZERO;
-        } else {
-            *register_f |= Registers::FLAG_ZERO;
-        }
-
-        *register_f &= !Registers::FLAG_SUBTRACTION;
-        *register_f |= Registers::FLAG_HALFCARRY;
-
-        Finish {
-            pc_increment: 1,
-            elapsed_cycles,
-        }
-    }
-
-    fn swap(byte_to_swap: &mut u8, register_f: &mut u8, elapsed_cycles: u8) -> Finish {
-        let upper_4_bits = *byte_to_swap & 0xf0;
-        let lower_4_bits = *byte_to_swap & 0x0f;
-        *byte_to_swap = upper_4_bits >> 4;
-        *byte_to_swap |= lower_4_bits << 4;
-
-        if *byte_to_swap == 0 {
-            *register_f |= Registers::FLAG_ZERO;
-        } else {
-            *register_f &= !Registers::FLAG_ZERO;
-        }
-
-        *register_f &= !Registers::FLAG_SUBTRACTION;
-        *register_f &= !Registers::FLAG_HALFCARRY;
-        *register_f &= !Registers::FLAG_CARRY;
-
-        Finish {
-            pc_increment: 1,
-            elapsed_cycles,
-        }
-    }
-
-    pub fn ld_reg8_mem8(dst_register: &mut u8, src_memory: u8) -> Finish {
-        *dst_register = src_memory;
-        Finish {
-            pc_increment: 2,
-            elapsed_cycles: 8,
-        }
-    }
-
-    pub fn ld_reg8_reg8(dst_register: &mut u8, src_register: u8) -> Finish {
-        *dst_register = src_register;
-        Finish {
-            pc_increment: 1,   // Same as NOP
-            elapsed_cycles: 4, // Same as NOP
-        }
-    }
-
-    pub fn nop() -> Finish {
-        Finish {
-            pc_increment: 1,   // Same as ld_reg8_reg8
-            elapsed_cycles: 4, // Same as ld_reg8_reg8
-        }
-    }
-
-    pub fn dec_reg8(register_to_dec: &mut u8, register_f: &mut u8) -> Finish {
-        if subtraction_produces_u8_half_carry(*register_to_dec, 1, *register_f, false) {
-            *register_f |= Registers::FLAG_HALFCARRY;
-        } else {
-            *register_f &= !Registers::FLAG_HALFCARRY;
-        }
-
-        *register_to_dec = register_to_dec.wrapping_sub(1);
-
-        if *register_to_dec != 0 {
-            *register_f &= !Registers::FLAG_ZERO;
-        } else {
-            *register_f |= Registers::FLAG_ZERO;
-        }
-
-        *register_f |= Registers::FLAG_SUBTRACTION;
-
-        Finish {
-            pc_increment: 1,
-            elapsed_cycles: 4,
-        }
-    }
-
-    pub fn add_reg16(src: u16, dst_register: &mut u16, register_f: &mut u8) -> Finish {
-        // Check for 16-bit full carry
-        if i32::from(*dst_register) + i32::from(src) > 0xffff {
-            *register_f |= Registers::FLAG_CARRY;
-        } else {
-            *register_f &= !Registers::FLAG_CARRY;
-        }
-
-        // Check for 16-bit half carry
-        if (*dst_register & 0x0fff) + (src & 0x0fff) > 0x0fff {
-            *register_f |= Registers::FLAG_HALFCARRY;
-        } else {
-            *register_f &= !Registers::FLAG_HALFCARRY;
-        }
-
-        *register_f &= !Registers::FLAG_SUBTRACTION;
-
-        *dst_register += src;
-
-        Finish {
-            pc_increment: 1,
-            elapsed_cycles: 8,
-        }
-    }
-}
-
 pub struct Cpu {
     registers: Registers, // rwtodo maybe just put the registers in the cpu without wrapping them in a struct
     is_halted: bool,
@@ -448,7 +194,7 @@ impl Cpu {
 
             if self.registers.ime {
                 self.registers.ime = false;
-                self.stack_push(self.registers.pc, memory);
+                stack_push(self.registers.pc, &mut self.registers.sp, memory);
 
                 // rwtodo can i do this with a match?
                 if interrupts_to_handle & interrupt::FLAG_VBLANK != 0 {
@@ -473,20 +219,6 @@ impl Cpu {
                 *memory.direct_access(address::INTERRUPT_FLAGS) = requested_interrupts;
             }
         }
-    }
-
-    fn stack_push(&mut self, value_to_push: u16, memory: &mut Memory) {
-        let bytes = value_to_push.to_le_bytes();
-        self.registers.sp -= 2;
-
-        memory.write(self.registers.sp, bytes[0]);
-        memory.write(self.registers.sp + 1, bytes[1]);
-    }
-
-    fn stack_pop(&mut self, memory: &Memory) -> u16 {
-        let popped_value = memory.read_u16(self.registers.sp);
-        self.registers.sp += 2;
-        popped_value
     }
 
     #[must_use] // Returns the number of cycles the instruction took.
@@ -709,6 +441,7 @@ impl Cpu {
                     elapsed_cycles: 16,
                 }
             } // JP xx
+            0xcd => call_condition(true, &mut self.registers.pc, &mut self.registers.sp, memory), // CALL xx
             0xe0 => {
                 memory.write(
                     0xff00 + u16::from(memory.read(self.registers.pc + 1)),
@@ -720,7 +453,7 @@ impl Cpu {
                 }
             } // LDH (ff00+x),A
             0xe1 => {
-                let popped_value = self.stack_pop(memory);
+                let popped_value = stack_pop(&mut self.registers.sp, memory);
                 self.registers.set_hl(popped_value);
                 Finish {
                     pc_increment: 1,
@@ -737,7 +470,7 @@ impl Cpu {
             0xe3 => unreachable!("Invalid opcode"),
             0xe4 => unreachable!("Invalid opcode"),
             0xe5 => {
-                self.stack_push(self.registers.hl(), memory);
+                stack_push(self.registers.hl(), &mut self.registers.sp, memory);
                 Finish {
                     pc_increment: 1,
                     elapsed_cycles: 16,
---
+++ src/cpu/instructions.rs
@@ -0,0 +1,275 @@
+use crate::cpu::*;
+
+pub struct Finish {
+    pub pc_increment: i16,
+    pub elapsed_cycles: u8,
+}
+
+pub fn subtraction_produces_u8_full_carry(a: u8, b: u8) -> bool {
+    i16::from(a) - i16::from(b) < 0
+}
+
+pub fn addition_produces_u8_full_carry(a: u8, b: u8) -> bool {
+    i16::from(a) + i16::from(b) > 0xff
+}
+
+pub fn subtraction_produces_u8_half_carry(
+    a: u8,
+    b: u8,
+    register_f: u8,
+    include_carry: bool,
+) -> bool {
+    let optional_carry: i16 = if include_carry && (register_f & Registers::FLAG_CARRY) != 0 {
+        1
+    } else {
+        0
+    };
+
+    i16::from(a & 0x0f) - i16::from(b & 0x0f) - optional_carry < 0
+}
+
+pub fn addition_produces_u8_half_carry(a: u8, b: u8, register_f: u8, include_carry: bool) -> bool {
+    let optional_carry: i16 = if include_carry && (register_f & Registers::FLAG_CARRY) != 0 {
+        1
+    } else {
+        0
+    };
+
+    i16::from(a & 0x0f) + i16::from(b & 0x0f) + optional_carry > 0x0f
+}
+
+pub fn inc_u8(value_to_increment: &mut u8, register_f: &mut u8, elapsed_cycles: u8) -> Finish {
+    if addition_produces_u8_half_carry(*value_to_increment, 1, *register_f, false) {
+        *register_f |= Registers::FLAG_HALFCARRY;
+    } else {
+        *register_f &= !Registers::FLAG_HALFCARRY;
+    }
+
+    *value_to_increment += 1;
+
+    if *value_to_increment != 0 {
+        *register_f &= !Registers::FLAG_ZERO;
+    } else {
+        *register_f |= Registers::FLAG_ZERO;
+    }
+
+    *register_f &= !Registers::FLAG_SUBTRACTION;
+
+    Finish {
+        pc_increment: 1,
+        elapsed_cycles,
+    }
+}
+
+pub fn xor(xor_input: u8, register_a: &mut u8, register_f: &mut u8, elapsed_cycles: u8) -> Finish {
+    *register_a ^= xor_input;
+
+    if *register_a == 0 {
+        *register_f |= Registers::FLAG_ZERO;
+    } else {
+        *register_f &= !Registers::FLAG_ZERO;
+    }
+
+    *register_f &= !Registers::FLAG_SUBTRACTION;
+    *register_f &= !Registers::FLAG_HALFCARRY;
+    *register_f &= !Registers::FLAG_CARRY;
+
+    Finish {
+        pc_increment: 1,
+        elapsed_cycles,
+    }
+}
+
+fn or(
+    or_input: u8,
+    register_a: &mut u8,
+    register_f: &mut u8,
+    pc_increment: i16,
+    elapsed_cycles: u8,
+) -> Finish {
+    // rwtodo: I think pc_increment might always be 1, thereby allowing us to remove it as from the param list.
+
+    *register_a |= or_input;
+
+    if *register_a == 0 {
+        *register_f |= Registers::FLAG_ZERO;
+    } else {
+        *register_f &= !Registers::FLAG_ZERO;
+    }
+
+    *register_f &= !Registers::FLAG_SUBTRACTION;
+    *register_f &= !Registers::FLAG_HALFCARRY;
+    *register_f &= !Registers::FLAG_CARRY;
+
+    Finish {
+        pc_increment,
+        elapsed_cycles,
+    }
+}
+
+pub fn and(
+    and_input: u8,
+    register_a: &mut u8,
+    register_f: &mut u8,
+    pc_increment: i16,
+    elapsed_cycles: u8,
+) -> Finish {
+    *register_a &= and_input;
+
+    if *register_a == 0 {
+        *register_f |= Registers::FLAG_ZERO;
+    } else {
+        *register_f &= !Registers::FLAG_ZERO;
+    }
+
+    *register_f &= !Registers::FLAG_SUBTRACTION;
+    *register_f |= Registers::FLAG_HALFCARRY;
+    *register_f &= !Registers::FLAG_CARRY;
+
+    Finish {
+        pc_increment,
+        elapsed_cycles,
+    }
+}
+
+fn set(bit_to_set: u8, byte_to_set: &mut u8, elapsed_cycles: u8) -> Finish {
+    *byte_to_set |= 0x01 << bit_to_set;
+    Finish {
+        pc_increment: 1,
+        elapsed_cycles,
+    }
+}
+
+fn res(bit_to_reset: u8, byte_to_reset: &mut u8, elapsed_cycles: u8) -> Finish {
+    *byte_to_reset &= !(0x01 << bit_to_reset);
+    Finish {
+        pc_increment: 1,
+        elapsed_cycles,
+    }
+}
+
+fn bit(bit_to_check: u8, byte_to_check: u8, register_f: &mut u8, elapsed_cycles: u8) -> Finish {
+    if (byte_to_check & (0x01 << bit_to_check)) != 0 {
+        *register_f &= !Registers::FLAG_ZERO;
+    } else {
+        *register_f |= Registers::FLAG_ZERO;
+    }
+
+    *register_f &= !Registers::FLAG_SUBTRACTION;
+    *register_f |= Registers::FLAG_HALFCARRY;
+
+    Finish {
+        pc_increment: 1,
+        elapsed_cycles,
+    }
+}
+
+fn swap(byte_to_swap: &mut u8, register_f: &mut u8, elapsed_cycles: u8) -> Finish {
+    let upper_4_bits = *byte_to_swap & 0xf0;
+    let lower_4_bits = *byte_to_swap & 0x0f;
+    *byte_to_swap = upper_4_bits >> 4;
+    *byte_to_swap |= lower_4_bits << 4;
+
+    if *byte_to_swap == 0 {
+        *register_f |= Registers::FLAG_ZERO;
+    } else {
+        *register_f &= !Registers::FLAG_ZERO;
+    }
+
+    *register_f &= !Registers::FLAG_SUBTRACTION;
+    *register_f &= !Registers::FLAG_HALFCARRY;
+    *register_f &= !Registers::FLAG_CARRY;
+
+    Finish {
+        pc_increment: 1,
+        elapsed_cycles,
+    }
+}
+
+pub fn ld_reg8_mem8(dst_register: &mut u8, src_memory: u8) -> Finish {
+    *dst_register = src_memory;
+    Finish {
+        pc_increment: 2,
+        elapsed_cycles: 8,
+    }
+}
+
+pub fn ld_reg8_reg8(dst_register: &mut u8, src_register: u8) -> Finish {
+    *dst_register = src_register;
+    Finish {
+        pc_increment: 1,   // Same as NOP
+        elapsed_cycles: 4, // Same as NOP
+    }
+}
+
+pub fn nop() -> Finish {
+    Finish {
+        pc_increment: 1,   // Same as ld_reg8_reg8
+        elapsed_cycles: 4, // Same as ld_reg8_reg8
+    }
+}
+
+pub fn dec_reg8(register_to_dec: &mut u8, register_f: &mut u8) -> Finish {
+    if subtraction_produces_u8_half_carry(*register_to_dec, 1, *register_f, false) {
+        *register_f |= Registers::FLAG_HALFCARRY;
+    } else {
+        *register_f &= !Registers::FLAG_HALFCARRY;
+    }
+
+    *register_to_dec = register_to_dec.wrapping_sub(1);
+
+    if *register_to_dec != 0 {
+        *register_f &= !Registers::FLAG_ZERO;
+    } else {
+        *register_f |= Registers::FLAG_ZERO;
+    }
+
+    *register_f |= Registers::FLAG_SUBTRACTION;
+
+    Finish {
+        pc_increment: 1,
+        elapsed_cycles: 4,
+    }
+}
+
+pub fn add_reg16(src: u16, dst_register: &mut u16, register_f: &mut u8) -> Finish {
+    // Check for 16-bit full carry
+    if i32::from(*dst_register) + i32::from(src) > 0xffff {
+        *register_f |= Registers::FLAG_CARRY;
+    } else {
+        *register_f &= !Registers::FLAG_CARRY;
+    }
+
+    // Check for 16-bit half carry
+    if (*dst_register & 0x0fff) + (src & 0x0fff) > 0x0fff {
+        *register_f |= Registers::FLAG_HALFCARRY;
+    } else {
+        *register_f &= !Registers::FLAG_HALFCARRY;
+    }
+
+    *register_f &= !Registers::FLAG_SUBTRACTION;
+
+    *dst_register += src;
+
+    Finish {
+        pc_increment: 1,
+        elapsed_cycles: 8,
+    }
+}
+
+pub fn call_condition(condition: bool, pc: &mut u16, sp: &mut u16, memory: &mut Memory) -> Finish {
+    if condition {
+        stack_push(*pc + 3, sp, memory);
+
+        *pc = memory.read_u16(*pc + 1);
+        Finish {
+            pc_increment: 0,
+            elapsed_cycles: 24,
+        }
+    } else {
+        Finish {
+            pc_increment: 3,
+            elapsed_cycles: 12,
+        }
+    }
+}
