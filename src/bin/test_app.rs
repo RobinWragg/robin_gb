1a34f8d3c542ff791928947383150d80ac96dbfa
Robin Wragg <robin.wragg@me.com>
2024-03-27 20:53:44 +0000

    Add some more wgpu init code

--- src/bin/test_app.rs
+++ src/bin/test_app.rs
@@ -3,11 +3,68 @@ use std::fs;
 use wgpu;
 use winit::event::{Event, WindowEvent};
 use winit::event_loop::{ControlFlow, EventLoop};
-use winit::window::WindowBuilder;
+use winit::window::{Window, WindowBuilder};
 
 // rwtodo: This will become a simple app that loads multiple instances of the emulator and loads a different game in each one.
 
 // rwtodo: Put this and winit/wgpu behind a feature, as I don't want users of the robin_gb library to have to download them.
+
+const WINDOW_WIDTH: u32 = 500;
+const WINDOW_HEIGHT: u32 = 300;
+
+async fn wgpu_init(window: &Window) {
+    let instance = wgpu::Instance::new(wgpu::InstanceDescriptor {
+        backends: wgpu::Backends::all(),
+        ..Default::default()
+    });
+
+    let surface = instance.create_surface(window).unwrap();
+
+    let adapter = instance
+        .request_adapter(&wgpu::RequestAdapterOptions {
+            power_preference: wgpu::PowerPreference::default(),
+            compatible_surface: Some(&surface),
+            force_fallback_adapter: false,
+        })
+        .await
+        .unwrap();
+
+    let (device, queue) = adapter
+        .request_device(
+            &wgpu::DeviceDescriptor {
+                required_features: wgpu::Features::empty(),
+                // WebGL doesn't support all of wgpu's features, so if
+                // we're building for the web, we'll have to disable some.
+                required_limits: wgpu::Limits::downlevel_webgl2_defaults(),
+                label: None,
+            },
+            None, // Trace path
+        )
+        .await
+        .unwrap();
+
+    // rwtodo: wgpu::Surface::get_default_config?
+    let surface_caps = surface.get_capabilities(&adapter);
+    // rwtodo this chaining is kinda smelly.
+    let surface_format = surface_caps
+        .formats
+        .iter()
+        .copied()
+        .filter(|f| f.is_srgb())
+        .next()
+        .unwrap_or(surface_caps.formats[0]);
+    let config = wgpu::SurfaceConfiguration {
+        usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
+        format: surface_format,
+        width: WINDOW_WIDTH,
+        height: WINDOW_HEIGHT,
+        present_mode: wgpu::PresentMode::Fifo,
+        alpha_mode: surface_caps.alpha_modes[0],
+        view_formats: vec![],
+        desired_maximum_frame_latency: 1,
+    };
+    surface.configure(&device, &config);
+}
 
 fn main() {
     println!("Hello, world!");
@@ -18,18 +75,7 @@ fn main() {
     event_loop.set_control_flow(ControlFlow::Poll);
     let window = WindowBuilder::new().build(&event_loop).unwrap();
 
-    let instance = wgpu::Instance::new(wgpu::InstanceDescriptor {
-        backends: wgpu::Backends::all(),
-        ..Default::default()
-    });
-
-    let surface = instance.create_surface(&window).unwrap();
-
-    let adapter = instance.request_adapter(&wgpu::RequestAdapterOptions {
-        power_preference: wgpu::PowerPreference::default(),
-        compatible_surface: Some(&surface),
-        force_fallback_adapter: false,
-    });
+    wgpu_init(&window);
 
     let _ = event_loop.run(move |event, elwt| match event {
         Event::WindowEvent {
