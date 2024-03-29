6898881404ec302fa9340ea83fbbc09d8f9889c3
Robin Wragg <robin.wragg@me.com>
2024-03-29 13:29:34 +0000

    Clear screen of test app

--- src/bin/test_app.rs
+++ src/bin/test_app.rs
@@ -1,6 +1,8 @@
+use futures::executor::block_on;
 use robin_gb;
 use std::fs;
 use wgpu;
+use winit::dpi::PhysicalSize;
 use winit::event::{Event, WindowEvent};
 use winit::event_loop::{ControlFlow, EventLoop};
 use winit::window::{Window, WindowBuilder};
@@ -9,10 +11,10 @@ use winit::window::{Window, WindowBuilder};
 
 // rwtodo: Put this and winit/wgpu behind a feature, as I don't want users of the robin_gb library to have to download them.
 
-const WINDOW_WIDTH: u32 = 500;
-const WINDOW_HEIGHT: u32 = 300;
+const WINDOW_WIDTH: u32 = 160 * 4;
+const WINDOW_HEIGHT: u32 = 144 * 4;
 
-async fn wgpu_init(window: &Window) {
+async fn wgpu_init(window: &Window) -> (wgpu::Surface, wgpu::Device, wgpu::Queue) {
     let instance = wgpu::Instance::new(wgpu::InstanceDescriptor {
         backends: wgpu::Backends::all(),
         ..Default::default()
@@ -33,12 +35,10 @@ async fn wgpu_init(window: &Window) {
         .request_device(
             &wgpu::DeviceDescriptor {
                 required_features: wgpu::Features::empty(),
-                // WebGL doesn't support all of wgpu's features, so if
-                // we're building for the web, we'll have to disable some.
                 required_limits: wgpu::Limits::downlevel_webgl2_defaults(),
                 label: None,
             },
-            None, // Trace path
+            None,
         )
         .await
         .unwrap();
@@ -64,18 +64,61 @@ async fn wgpu_init(window: &Window) {
         desired_maximum_frame_latency: 1,
     };
     surface.configure(&device, &config);
+
+    (surface, device, queue)
+}
+
+fn render(surface: &wgpu::Surface, device: &wgpu::Device, queue: &wgpu::Queue) {
+    let output = surface.get_current_texture().unwrap();
+    let view = output
+        .texture
+        .create_view(&wgpu::TextureViewDescriptor::default());
+
+    let mut encoder =
+        device.create_command_encoder(&wgpu::CommandEncoderDescriptor { label: None });
+
+    {
+        let clear_color = wgpu::Color {
+            r: 0.2,
+            g: 1.0,
+            b: 0.1,
+            a: 1.0,
+        };
+        encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+            label: None,
+            color_attachments: &[Some(wgpu::RenderPassColorAttachment {
+                view: &view,
+                resolve_target: None,
+                ops: wgpu::Operations {
+                    load: wgpu::LoadOp::Clear(clear_color),
+                    store: wgpu::StoreOp::Store,
+                },
+            })],
+            depth_stencil_attachment: None,
+            occlusion_query_set: None,
+            timestamp_writes: None,
+        });
+    }
+
+    queue.submit(std::iter::once(encoder.finish()));
+    output.present();
 }
 
 fn main() {
-    println!("Hello, world!");
     let rom_file_data = fs::read("roms/Tetris.gb").unwrap();
     let mut gb = robin_gb::GameBoy::new(&rom_file_data[..]);
 
     let event_loop = EventLoop::new().unwrap();
     event_loop.set_control_flow(ControlFlow::Poll);
-    let window = WindowBuilder::new().build(&event_loop).unwrap();
 
-    wgpu_init(&window);
+    let size = PhysicalSize::new(WINDOW_WIDTH, WINDOW_HEIGHT);
+    let window = WindowBuilder::new()
+        .with_title("robin_gb")
+        .with_inner_size(size)
+        .build(&event_loop)
+        .unwrap();
+
+    let (surface, device, queue) = block_on(wgpu_init(&window));
 
     let _ = event_loop.run(move |event, elwt| match event {
         Event::WindowEvent {
@@ -87,6 +130,7 @@ fn main() {
         }
         Event::AboutToWait => {
             let _ = gb.emulate_next_frame();
+            render(&surface, &device, &queue);
         }
         _ => (),
     });
