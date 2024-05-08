86f8fe43e07d79134d4a7cd2c048b90a8057d57f
Robin Wragg <robin.wragg@me.com>
2024-05-08 22:26:01 +0100

    Implement SLA instruction

--- src/cpu/instructions/cb.rs
+++ src/cpu/instructions/cb.rs
@@ -17,6 +17,7 @@ pub fn execute_cb_instruction(registers: &mut Registers, memory: &mut Memory) ->
 
     let flag_diff = match immediate_byte {
         0x10..=0x17 => rl(&mut operand, registers.f & Registers::FLAG_CARRY != 0),
+        0x20..=0x27 => sla(&mut operand),
         0x30..=0x37 => swap(&mut operand),
         0x38..=0x3f => srl(&mut operand),
         0x40..=0x7f => bit(operand, bit_index),
@@ -60,6 +61,20 @@ fn rl(byte_to_rotate: &mut u8, previous_carry_flag: bool) -> FlagDiff {
         n: Some(false),
         h: Some(false),
         c: Some(new_carry_flag),
+    }
+}
+
+fn sla(byte_to_shift: &mut u8) -> FlagDiff {
+    let carry = *byte_to_shift & make_bit(7) != 0;
+
+    *byte_to_shift <<= 1;
+    *byte_to_shift &= !make_bit(0); // bit 0 should become 0.
+
+    FlagDiff {
+        z: Some(*byte_to_shift == 0),
+        n: Some(false),
+        h: Some(false),
+        c: Some(carry),
     }
 }
 
