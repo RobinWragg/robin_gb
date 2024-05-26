8f3333027d49de4fe274bf8f249de44f60697bcd
Robin Wragg <robin.wragg@me.com>
2024-05-26 14:37:41 +0100

    Return HTML table from ci_rom_test

--- src/bin/ci_rom_test.rs
+++ src/bin/ci_rom_test.rs
@@ -1,7 +1,6 @@
-// This file produces a binary that loads multiple roms and emulates them simultaneously,
-// rendering them in a grid using wgpu.
+// This file produces a binary that receives rom files, collects their serial output, and builds an HTML table of the results with ✅ or ❌.
 
-use clap::{builder::OsStr, Parser};
+use clap::Parser;
 use robin_gb::GameBoy;
 use std::path::PathBuf;
 
@@ -13,27 +12,49 @@ struct CliArgs {
     roms: Vec<PathBuf>,
 }
 
+fn run_rom_test(path: PathBuf) -> Result<String, String> {
+    // Validate the path.
+    let path_str = path.to_str().ok_or("Path does not convert to &str")?;
+    if !path.is_file() {
+        return Err(format!("Path is not a file: {}", path_str));
+    }
+    let extension = path.extension().and_then(std::ffi::OsStr::to_str);
+    let extension = extension.ok_or(format!("No extension found for path: {}", path_str))?;
+    if extension.to_lowercase() != "gb" {
+        return Err(format!("Expected extension 'gb' for path: {}", path_str));
+    }
+
+    Ok("Path is valid. (rom test TODO)".to_owned())
+}
+
+fn sanitize_html_text(text: &str) -> String {
+    let text = text.replace("&", "&amp");
+    let text = text.replace("<", "&lt");
+    let text = text.replace(">", "&gt");
+    let text = text.replace("\"", "&quot");
+    text.replace("'", "&#39")
+}
+
 fn main() {
     let args = CliArgs::parse();
 
+    let mut html =
+        String::from("<html>\n<head>\n<meta charset=\"UTF-8\">\n</head>\n<body>\n<table>\n");
+
     for path in args.roms {
-        let path_str = path.to_str().expect("Path does not convert to str");
-        assert_eq!(path.exists(), true, "Path does not exist: {}", path_str);
-        assert_eq!(path.is_file(), true, "Path is not a file: {}", path_str);
-        let extension = path.extension().and_then(std::ffi::OsStr::to_str);
-        let extension = extension.unwrap_or_else(|| panic!("No extension found: {}", path_str));
-        assert_eq!(extension.to_lowercase(), "gb");
+        let name = sanitize_html_text(
+            path.file_name()
+                .and_then(std::ffi::OsStr::to_str)
+                .expect("Expected file name"),
+        );
+        let result = sanitize_html_text(&match run_rom_test(path) {
+            Ok(o) => format!("✅ {}", o),
+            Err(e) => format!("❌ {}", e),
+        });
+
+        html += &format!("<tr>\n<td>{}</td><td>{}</td>\n</tr>\n", name, result);
     }
-    // rwtodo: make this a command line argument.
-    // let path = fs::read_dir("roms/romonly").unwrap();
-    // let num_frames = 60 * 60; // 1 minute.
 
-    // rwtodo: Load the rom, and emulate for the set time. Then collect the serial data.
-
-    // Boot up the game boy.
-    // let game_boy = GameBoy::new(&rom);
-
-    // for i in 0..num_frames {
-    //     let _ = game_boy.emulate_next_frame();
-    // }
+    html += "</table>\n</body>\n</html>";
+    println!("{}", html);
 }
